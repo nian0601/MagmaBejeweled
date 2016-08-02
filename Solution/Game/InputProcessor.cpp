7cea7afa9f0153857b38d6627e62a36763b7a9a4
nian0601 <nian0601skola@hotmail.se>
2016-08-02 23:13:30 +0200

    Added more rules to InputProcessor: - Can now only swap Gems that lead to a 3-match (they still dont get destroyed)

--- Solution/Game/InputProcessor.cpp
+++ Solution/Game/InputProcessor.cpp
@@ -34,7 +34,7 @@ void InputProcessor::Update(float)
 		}
 		else if (myFirstEntity != clickedEntity)
 		{
-			if (IsAdjacent(myFirstEntity, clickedEntity))
+			if (IsAdjacent(myFirstEntity, clickedEntity) && LegalMove(myFirstEntity, clickedEntity))
 			{
 				AddComponent<MovementComponent>(myFirstEntity);
 				AddComponent<MovementComponent>(clickedEntity);
@@ -95,4 +95,62 @@ bool InputProcessor::IsAdjacent(Magma::Entity aFirstEntity, Magma::Entity aSecon
 	}
 
 	return false;
+}
+
+bool InputProcessor::LegalMove(Magma::Entity aFirstEntity, Magma::Entity aSecondEntity)
+{
+	GemComponent& firstGem = GetComponent<GemComponent>(aFirstEntity);
+	GemComponent& secondGem = GetComponent<GemComponent>(aSecondEntity);
+	CU::Vector2<int> swapDirection = firstGem.myIndex - secondGem.myIndex;
+
+	eGemType temp = firstGem.myType;
+	firstGem.myType = secondGem.myType;
+	secondGem.myType = temp;
+
+	int horizontalOne = GetGemCount(firstGem, firstGem.myType, { -1, 0 }) + GetGemCount(firstGem, firstGem.myType, { 1, 0 }) + 1;
+	int horizontalTwo = GetGemCount(secondGem, secondGem.myType, { -1, 0 }) + GetGemCount(secondGem, secondGem.myType, { 1, 0 }) + 1;
+
+	int verticalOne = GetGemCount(firstGem, firstGem.myType, { 0, -1 }) + GetGemCount(firstGem, firstGem.myType, { 0, 1 }) + 1;
+	int verticalTwo = GetGemCount(secondGem, secondGem.myType, { 0, -1 }) + GetGemCount(secondGem, secondGem.myType, { 0, 1 }) + 1;
+
+	temp = firstGem.myType;
+	firstGem.myType = secondGem.myType;
+	secondGem.myType = temp;
+
+	return (verticalOne >= 3 || verticalTwo >= 3) || (horizontalOne >= 3 || horizontalTwo >= 3);
+}
+
+Magma::Entity InputProcessor::GetEntity(const CU::Vector2<int>& aIndex)
+{
+	const CU::GrowingArray<Magma::Entity>& entities = GetEntities();
+
+	for each (Magma::Entity entity in entities)
+	{
+		GemComponent& gem = GetComponent<GemComponent>(entity);
+		if (gem.myIndex == aIndex)
+		{
+			return entity;
+		}
+	}
+
+	return static_cast<Magma::Entity>(-1);
+}
+
+int InputProcessor::GetGemCount(const GemComponent& aGem, eGemType aGemType, const CU::Vector2<int>& aDirection)
+{
+	CU::Vector2<int> newIndex = aGem.myIndex + aDirection;
+
+	Magma::Entity entity = GetEntity(newIndex);
+	if (entity == -1)
+	{
+		return 0;
+	}
+
+	const GemComponent& newGem = GetComponent<GemComponent>(entity);
+	if (newGem.myType != aGemType)
+	{
+		return 0;
+	}
+
+	return 1 + GetGemCount(newGem, aGemType, aDirection);
 }
--- Solution/Game/InputProcessor.h
+++ Solution/Game/InputProcessor.h
@@ -3,6 +3,8 @@
 #include <Entity/Include/BaseProcessor.h>
 #include <Entity/Include/CustomEntityTypes.h>
 
+#include "GemComponent.h"
+
 class InputProcessor : public Magma::BaseProcessor
 {
 public:
@@ -12,9 +14,14 @@ public:
 	void Update(float aDelta) override;
 
 	void OnEvent(const Magma::Event& aEvent) override;
+
 private:
 	Magma::Entity GetClickedEntity();
 	bool IsAdjacent(Magma::Entity aFirstEntity, Magma::Entity aSecondEntity);
+	bool LegalMove(Magma::Entity aFirstEntity, Magma::Entity aSecondEntity);
+	Magma::Entity GetEntity(const CU::Vector2<int>& aIndex);
+
+	int GetGemCount(const GemComponent& aGem, eGemType aGemType, const CU::Vector2<int>& aDirection);
 
 	Magma::Entity myFirstEntity;
 };
